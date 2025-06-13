3c91eaa03084c54982b5dd14906369fed049c251
Leo Knop <2122594@stud.hs-mannheim.de>
2025-06-13 19:42:42 +0200

    Fehler in pCB behoben durch OP

--- ch32fun/examples/uartdemo/uartdemo.c
+++ ch32fun/examples/uartdemo/uartdemo.c
@@ -8,24 +8,54 @@
 #include <stdio.h>
 
 uint32_t count;
+void timer_init(void){
+	// Reset TIM1 to init all regs
+	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+	
+	// CTLR1: default is up, events generated, edge align
+	// SMCFGR: default clk input is CK_INT
+	
+	// Prescaler 
+	TIM1->PSC = 0x0000;
+	
+	// Auto Reload - sets period
+	TIM1->ATRLR = 255;
+	
+	// Reload immediately
+	TIM1->SWEVGR |= TIM_UG;
+	
+	// Enable CH1N output, positive pol
+	TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
+	
+	// Enable CH4 output, positive pol
+	TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	
+	// CH1 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
+	
+	// CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+	TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1;
+	
+	// Set the Capture Compare Register value to 50% initially
+	TIM1->CH1CVR = 128;
+	TIM1->CH4CVR = 128;
+	
+	// Enable TIM1 outputs
+	TIM1->BDTR |= TIM_MOE;
+	
+	// Enable TIM1
+	TIM1->CTLR1 |= TIM_CEN;
+}
 
 int main()
 {
 	SystemInit();
 
-	// Enable GPIOD.
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD;
-
-	// GPIO D0 Push-Pull, 10MHz Output
-	GPIOD->CFGLR &= ~(0xf<<(4*0));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
-
+	
 	while(1)
 	{
-		GPIOD->BSHR = 1;	 // Turn on GPIOD0
-		Delay_Ms( 50 );
-		GPIOD->BSHR = 1<<16; // Turn off GPIOD0
-		Delay_Ms( 50 );
-		printf( "Count: %lu\n", count++ );
+		Delay_Ms(1000);
+		printf("1s vergabngen");
 	}
 }
